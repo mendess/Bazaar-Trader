9411f661b70fac8d36c8ee8a7776b5901b86109e
Pimonteiro <pimonteiro@protonmail.com>
2018-02-10 12:16:02 +0000

    more views

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,12 @@
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="description" content="">
+<meta name="author" content="Magic Team">
+
+<title>Bazaar Trading Center</title>
+
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('includes.head')
+</head>
+<body>
+    <div class="container">
+
+        <header class="row">
+            @include('includes.header')
+        </header>
+
+        <div id="main" class="row">
+
+                @yield('content')
+
+        </div>
+
+        <footer class="row">
+            @include('includes.footer')
+        </footer>
+
+    </div>
+</body>
+</html>
