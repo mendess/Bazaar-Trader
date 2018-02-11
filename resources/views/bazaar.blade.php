96b7759c202971e0b2df26be9e1bc030928d4f35
Mendess2526 <pedro.mendes.26@gmail.com>
2018-02-11 02:50:30 +0000

    Adds Km field

--- resources/views/bazaar.blade.php
+++ resources/views/bazaar.blade.php
@@ -2,19 +2,39 @@
 
 
 @section('content')
-
-<div>
-    <ul>
-        @foreach ($usersWant as $user)
-        <li><a href="/users/{{ $user->id }}">{{ $user->name }}</a></li>
-        @endforeach
-    </ul>
+<div class="panel-body">
+    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+            <label for="name" class="col-md-4 control-label">Km range</label>
+        
+            <div class="col-md-6">
+                <input id="copies" type="number" class="form-control" name="copies" value="{{ old('copies') }}" required> <!--ITS GOING DOWN -->
+        
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('copies') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
 </div>
-<div>
-    <ul>
-        @foreach ($usersHave as $user)
-        <li><a href="/users/{{ $user->id }}">{{ $user->name }}</a></li>
-        @endforeach
-    </ul>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p>Users that have cards you want</p>
+            <ul>
+                @foreach ($usersWant as $user)
+                <li><a href="/users/{{ $user->id }}">{{ $user->name }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="col">
+            <p>Users that want cards you have</p>
+            <ul>
+                @foreach ($usersHave as $user)
+                <li><a href="/users/{{ $user->id }}">{{ $user->name }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </div>
 @endsection
